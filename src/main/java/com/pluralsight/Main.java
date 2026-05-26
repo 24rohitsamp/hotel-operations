44c1db2a947cd6f3831271e8e4ead85dabcc6b03
24rohitsamp <24rohitsamp@gmail.com>
2026-05-26 14:57:00 -0400

    Fixed Commit Message

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,35 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Test Employee class
+        Employee employee = new Employee(1, "Alan Brock", "APPDEV", 50.00, 41.00);
+        System.out.println("Employee ID: " + employee.getEmployeeId());
+        System.out.println("Employee Name: " + employee.getName());
+        System.out.println("Employee Department: " + employee.getDepartment());
+        System.out.println("Employee Pay Rate: " + employee.getPayRate());
+        System.out.println("Employee Hours Worked: " + employee.getHoursWorked());
+        System.out.println("Employee Total Pay: " + employee.getTotalPay());
+        System.out.println("Employee Regular Hours: " + employee.getRegularHours());
+        System.out.println("Employee Overtime Hours: " + employee.getOvertimeHours());
+        System.out.println("=======================================================================");
+
+        // Test Reservation class
+        Reservation reservation = new Reservation("king", 3, true);
+        System.out.println("Room Type: " + reservation.getRoomType());
+        System.out.println("Price per Night: " + reservation.getPrice());
+        System.out.println("Number of Nights: " + reservation.getNumberOfNights());
+        System.out.println("Is Weekend: " + reservation.isWeekend());
+        System.out.println("Reservation Total: " + reservation.getReservationTotal());
+        System.out.println("=======================================================================");
+
+        // Test Room class
+        Room room = new Room(2, 99.99);
+        System.out.println("Number of Beds: " + room.getNumberOfBeds());
+        System.out.println("Price: " + room.getPrice());
+        System.out.println("Is Occupied: " + room.isOccupied());
+        System.out.println("Is Dirty: " + room.isDirty());
+        System.out.println("Is Available: " + room.isAvailable());
     }
 }
--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,58 @@
 package com.pluralsight;
 
 public class Reservation {
+
+    private String roomType;       // "king" or "double"
+    private int numberOfNights;
+    private boolean weekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean weekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.weekend = weekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return weekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.weekend = isWeekend;
+    }
+
+    public double getPrice() {
+        double basePrice;
+
+        if (roomType.equalsIgnoreCase("king")) {
+            basePrice = 139.00;
+        } else {
+            basePrice = 124.00;
+        }
+
+        if (weekend) {
+            basePrice *= 1.10;
+        }
+
+        return Math.round(basePrice * 100.0) / 100.0;
+    }
+
+    public double getReservationTotal() {
+        return Math.round(getPrice() * numberOfNights * 100.0) / 100.0;
+    }
 }
